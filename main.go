46f800b12a34d21b36ce408b69d3a1d8a8f61f88
ShaB <shab@cloud66.com>
2015-05-29 16:18:40 +0100

    use net.resolver instead of git

--- main.go
+++ main.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"io"
+	"net"
 	"net/http"
-	"os/exec"
+	//	"os/exec"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	var (
-		cmdOut []byte
-		err    error
-	)
+	/*	var (
+			cmdOut []byte
+			err    error
+		)
+	*/
 
 	hostname := r.URL.Query().Get("hostname")
 	if len(hostname) != 0 {
-		cmdName := "dig"
-		cmdArgs := []string{"@localhost", hostname}
+		/*		cmdName := "dig"
+				cmdArgs := []string{"@localhost", hostname}
 
-		if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+				if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+					io.WriteString(w, err.Error())
+				} else {
+					io.WriteString(w, string(cmdOut))
+				}*/
+
+		if ip, err := net.ResolveIPAddr("ip4", hostname); err != nil {
 			io.WriteString(w, err.Error())
 		} else {
-			io.WriteString(w, string(cmdOut))
+			io.WriteString(w, ip.String())
 		}
-		//io.WriteString(w, hostname)
+
 	} else {
 		io.WriteString(w, "There is no hostname !")
 	}
