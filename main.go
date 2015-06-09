20766390507266822a01018f6105cabfa11096e8
Vic van Gool <vic@cloud66.com>
2015-06-09 18:44:40 +0100

    added logging

--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"io"
 	"net"
 	"net/http"
-	//	"os/exec"
+	"os"
+
+	"github.com/cloud66/pink-panther/logger"
+	log "gopkg.in/inconshreveable/log15.v2"
 )
 
+var Log = log.New()
+
 func hello(w http.ResponseWriter, r *http.Request) {
+
 	/*	var (
 			cmdOut []byte
 			err    error
 		)
 	*/
+	Log.Info("Hello Request: START")
 
 	hostname := r.URL.Query().Get("hostname")
 	if len(hostname) != 0 {
@@ -27,17 +34,43 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 		if ip, err := net.ResolveIPAddr("ip4", hostname); err != nil {
 			io.WriteString(w, err.Error())
+			Log.Info("Hello Request: Error: " + err.Error())
 		} else {
 			io.WriteString(w, ip.String())
+			Log.Info("Hello Request:" + ip.String())
 		}
 
 	} else {
 		io.WriteString(w, "There is no hostname !")
+		Log.Info("Hello Request: There is no hostname!")
 	}
+
+	Log.Info("Hello Request: END")
 
 }
 
 func main() {
+	Log.SetHandler(log.StdoutHandler)
+
+	defer func() {
+		if r := recover(); r != nil {
+			Log.Info("Unhandled Error!")
+			err, ok := r.(error)
+			if ok {
+				Log.Info("FATAL: Unhandled Error! " + err.Error())
+				logger.Log.Crit(err)
+			} else {
+				Log.Info("FATAL: Unhandled Error!")
+			}
+			os.Exit(2)
+			return
+		}
+	}()
+
+	Log.Info("Web started!")
+
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8000", nil)
+
+	Log.Info("Web stopping!")
 }
