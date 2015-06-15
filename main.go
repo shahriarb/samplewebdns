1f63a09dfbaa2cdaff7e7d447b590fcaf3f5bee7
ShaB <shab@cloud66.com>
2015-06-15 18:39:27 +0100

    remove log trapping

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
+	//	"os"
+	//	"os/signal"
+	//	"syscall"
 
 	log "gopkg.in/inconshreveable/log15.v2"
 )
@@ -15,20 +15,21 @@ import (
 var Log = log.New()
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		if r := recover(); r != nil {
-			Log.Info("Unhandled Error!")
-			err, ok := r.(error)
-			if ok {
-				Log.Info("FATAL: Unhandled Error! " + err.Error())
-			} else {
-				Log.Info("FATAL: Unhandled Error!")
+	/*
+		defer func() {
+			if r := recover(); r != nil {
+				Log.Info("Unhandled Error!")
+				err, ok := r.(error)
+				if ok {
+					Log.Info("FATAL: Unhandled Error! " + err.Error())
+				} else {
+					Log.Info("FATAL: Unhandled Error!")
+				}
+				os.Exit(2)
+				return
 			}
-			os.Exit(2)
-			return
-		}
-	}()
-
+		}()
+	*/
 	Log.Info("Hello Request: START")
 
 	hostname := r.URL.Query().Get("hostname")
@@ -60,34 +61,35 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	Log.SetHandler(log.StdoutHandler)
 
-	// listen to signals
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	/*
+		// listen to signals
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	defer func() {
-		if r := recover(); r != nil {
-			Log.Info("Unhandled Error!")
-			err, ok := r.(error)
-			if ok {
-				Log.Info("FATAL: Unhandled Error! " + err.Error())
-			} else {
-				Log.Info("FATAL: Unhandled Error!")
-			}
-			os.Exit(2)
-			return
-		}
-	}()
-
-	go func() {
-		for {
-			select {
-			case sig := <-signalChan:
-				Log.Info("Signal! (" + sig.String() + ")")
+		defer func() {
+			if r := recover(); r != nil {
+				Log.Info("Unhandled Error!")
+				err, ok := r.(error)
+				if ok {
+					Log.Info("FATAL: Unhandled Error! " + err.Error())
+				} else {
+					Log.Info("FATAL: Unhandled Error!")
+				}
+				os.Exit(2)
 				return
 			}
-		}
-	}()
+		}()
 
+		go func() {
+			for {
+				select {
+				case sig := <-signalChan:
+					Log.Info("Signal! (" + sig.String() + ")")
+					return
+				}
+			}
+		}()
+	*/
 	Log.Info("Web started!")
 
 	http.HandleFunc("/", hello)
